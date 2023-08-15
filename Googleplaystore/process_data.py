bd848508abb1d2760786f53e5bec60bf9caadd30
Charilaos Alkiviades Savoullis <casavoullis@gmail.com>
2023-08-15 22:11:00 +0100

    converting the ipynb to .py for flask api use

---
+++ Googleplaystore/process_data.py
@@ -0,0 +1,90 @@
+# process_data.py
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, regexp_replace
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, FloatType
+
+class DataProcessor:
+    """
+    This class processes a CSV file using PySpark.
+    It loads the data, performs cleaning operations, and saves the processed data to a new CSV file.
+    The explanation of the processing of the GooglePlayStore.csv file is in:
+    PySpark_ML_GooglePlayStore.ipynb.
+    """
+
+    def __init__(self, input_path, output_path=None):
+        """
+        Initializes the DataProcessor object.
+
+        Parameters:
+            - input_path (str): The path to the input CSV file.
+            - output_path (str, optional): The path to save the processed CSV file. If not provided, the data won't be saved.
+        """
+        self.input_path = input_path
+        self.output_path = output_path
+        self.spark = self._create_spark_session()
+
+    def _create_spark_session(self):
+        """
+        Creates a Spark session.
+
+        Returns:
+            - SparkSession: The Spark session.
+        """
+        return SparkSession.builder.appName("DataProcessing").getOrCreate()
+
+    def _load_data(self):
+        """
+        Loads the CSV data into a PySpark DataFrame.
+
+        Returns:
+            - DataFrame: The loaded DataFrame.
+        """
+        # Define the schema for the DataFrame
+        schema = StructType([
+            StructField("App", StringType(), True),
+            StructField("Category", StringType(), True),
+            StructField("Rating", StringType(), True),
+            StructField("Reviews", StringType(), True),
+            StructField("Installs", StringType(), True),
+            StructField("Type", StringType(), True),
+            StructField("Price", StringType(), True),
+        ])
+        # Read the CSV file
+        return self.spark.read.csv(self.input_path, sep=',', escape='"', header=True, schema=schema)
+
+    def process_data(self):
+        """
+        Cleaning and Processesing the Dataframe.
+        """
+        # Load the data
+        df = self._load_data()
+
+        # Dropping Unnecessary columns
+        df_cleaned = df.drop("Size", "Content Rating", "Last Updated", "Android Ver", "Current Ver")
+
+        # Transforming the data into the correct data type
+        df_cleaned = df_cleaned.withColumn("Reviews", col("Reviews").cast(IntegerType())) \
+                    .withColumn("Installs", regexp_replace(col("Installs"), "[^0-9]", "").cast(IntegerType())) \
+                    .withColumn("Price", regexp_replace(col("Price"), "\\$", "").cast(FloatType())) 
+
+        # Droping Unnecessary rows
+        df_cleaned = df_cleaned.filter(~(col("Reviews").isNull() & col("Installs").isNull() & col("Price").isNull()))
+        df_cleaned = df_cleaned.filter(col("App") != "Command & Conquer: Rivals")
+
+        # Replace NaN values in the "Rating" column with 0
+        df_cleaned = df_cleaned.fillna(0, subset=["Rating"])
+
+        if self.output_path:
+            # Save the processed data to a new CSV file
+            df_cleaned.write.csv(self.output_path, header=True, mode="overwrite")
+
+        return df_cleaned
+
+if __name__ == "__main__":
+    input_file = "googleplaystore/googleplaystore.csv"
+    output_file = "processed_googleplaystore.csv"
+
+    processor = DataProcessor(input_file)
+    processed_df = processor.process_data()
+
+    print(processed_df.show(15))
